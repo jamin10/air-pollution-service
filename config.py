c378479cb6b19fd30ff8a4d9ca9f972e1262f4db
Ben Whitfield <bpwhitfield@outlook.com>
2022-09-30 10:05:17 +0100

    feature: get environement variables from file and validate

--- config.py
+++ config.py
@@ -1,47 +1,54 @@
 from dataclasses import dataclass
 import os
+from dotenv import load_dotenv
 
-from apt import ProblemResolver
+load_dotenv() # Load environ variables from .env 
 
 class ConfigError(Exception):
     pass
 
 @dataclass
 class Config:
+    db_name: str
     db_username: str
     db_password: str
+    host: str
+    port_id: int
     api_Key_geodb: str
     api_key_open_weather: str
 
     def validate(self):
-        # Todo: Make this validation throw Exception if Config is not present
-        if self.db_username == None:
+        # Validation to throw exception if config value is missing 
+        if self.db_name is None:
+            raise ConfigError("db_name is not set")
+        if self.db_username is None:
             raise ConfigError("db_username is not set")
-
-        if self.db_password == None:
+        if self.db_password is None:
            raise ConfigError("db_password is not set")
-        
-        if self.api_Key_geodb == None:
-            raise ConfigError("api_Key_geodb is not set")
-
-        if self.api_key_open_weather == None:
+        if self.db_password is None:
+           raise ConfigError("db_password is not set")
+        if self.host is None:
+           raise ConfigError("host is not set")
+        if self.port_id is None:
+            raise ConfigError("port_id is not set")
+        if self.api_Key_geodb is None:
+           raise ConfigError("api_Key_geodb is not set")
+        if self.api_key_open_weather is None:
             raise ConfigError("api_key_open_weather is not set")
 
         
-
 def load_config():
+    # Create Congif dataclass from enironment variables 
     config = Config(
+        os.environ.get("DB_NAME"),
         os.environ.get("DB_USERNAME"),
         os.environ.get("DB_PASSWORD"),
+        os.environ.get("HOST"),
+        os.environ.get("PORT_ID"),
         os.environ.get("API_KEY_GEODB"),
         os.environ.get("API_KEY_OPEN_WEATHER")
     )
+    
     config.validate()
-    return config
 
-    # Todo: Pass these as environment variables 
-    # GeoDB_API_key = 'b29f2328d6mshfe5d6639483b6a2p166f69jsn5c93c74e7d77'
-    # OW_API_key = '371506301b2009441f3135528150fb96'
-    # username = 'postgres'
-    # password = 'postgres'
-    #return dataclass called config 
+    return config
