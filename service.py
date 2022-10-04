6ab944a9bde93b46ee25a882dcc13149a8bb8412
Ben Whitfield <bpwhitfield@outlook.com>
2022-10-04 10:34:30 +0100

    Feature: configure client_pollution service

--- service.py
+++ service.py
@@ -1,10 +1,19 @@
+from client_cities import *
+
 class Service:
 
-    def __init__(self, client_cities, client_pollution, repository):
+    def __init__(self, client_cities, client_pollution):
         self.client_cities = client_cities
         self.client_pollution = client_pollution
-        self.reposiotry = repository
+        # self.reposiotry = repository
 
-    def run(self):
-        pass
-    
+    def run(self, params):
+        cities_data = self.client_cities.get_cities_data(params)
+        print(cities_data)
+
+        pollution_data = []
+        for city in cities_data:
+            pollution = self.client_pollution.get_pollution_data(city)
+            pollution_data.append(pollution)
+
+        print(pollution_data)
